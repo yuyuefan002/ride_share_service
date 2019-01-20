2113b854e5350875fe6692a4323bdb23b372a2bd
yuyuefan002 <yy225@vcm-5475.vm.duke.edu>
2019-01-19 23:04:26 -0500

    new models and admin

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,25 @@
-from django.contrib import admin
 
+from django.contrib import admin
+from .models import Request, Driver
 # Register your models here.
+
+
+class DriverInline(admin.TabularInline):
+    model = Driver
+    extra = 0
+
+
+@admin.register(Request)
+class RequestAdmin(admin.ModelAdmin):
+    # list_display = ['id', 'owner', 'driver', 'create_date', 'destination', 'arrival_time', 'passenger_num', 'share_or_not', 'special_car_info', 'remarks', 'status']
+    list_display = ['id', 'create_date', 'owner','status']
+    list_filter = ('status', 'passenger_num')
+    fieldsets = ((None, {'fields': ('id', 'create_date', 'status', 'share_or_not',)}),
+                 ('Passenger info', {'fields': ('owner', 'destination', 'arrival_time', 'passenger_num')}),
+                 ('Vehicle info', {'fields': ('type', 'special_car_info', 'remarks')}))
+    inlines = [DriverInline]
+
+
+@admin.register(Driver)
+class DriverAdmin(admin.ModelAdmin):
+    list_display = ['last_name', 'first_name', 'user', 'type', 'plate_number', 'max_passenger', 'special_car_info']
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,85 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
+from django.urls import reverse
+from django.utils import timezone
+from django.core.validators import MinValueValidator, MaxValueValidator
+import datetime
+
 
 # Create your models here.
+class Driver(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    request = models.OneToOneField('Request', on_delete=models.SET_NULL, null=True, blank=True)
+    VEHICLE_TYPE = (
+        ('sd', 'sedan'),
+        ('sv', 'SUV'),
+        ('vn', 'van'),
+        ('lx', 'Luxury'),
+    )
+    type = models.CharField(
+        max_length=2,
+        choices=VEHICLE_TYPE,
+        blank=True,
+        default='sd',
+        help_text='vehicle type')
+    plate_number = models.CharField(max_length=20)
+    max_passenger = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(6)])
+    special_car_info = models.TextField('special_car_info', null=True, blank=True, max_length=1000)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        return reverse('order:driver-info',args=[str(self.id)])
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
+
+
+class Request(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text='Unique ID for this particular request')
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    # sharer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    create_date = models.DateTimeField('Creation Date', default=timezone.now)
+    destination = models.CharField(max_length=200)
+    arrival_time = models.DateTimeField(null=True, blank=True)
+    passenger_num = models.IntegerField(default=1,validators=[MinValueValidator(1), MaxValueValidator(6)])
+    share_or_not = models.BooleanField(default=False)
+    VEHICLE_TYPE = (
+        ('sd', 'sedan'),
+        ('sv', 'SUV'),
+        ('vn', 'van'),
+        ('lx', 'Luxury'),
+    )
+    type = models.CharField(
+        max_length=2,
+        choices=VEHICLE_TYPE,
+        null=True,
+        blank=True,
+        help_text='vehicle type')
+    special_car_info = models.TextField('special_car_info', null=True, blank=True, max_length=1000)
+    remarks = models.TextField('remarks', null=True, blank=True, max_length=1000)
+    REQUEST_STATUS = (
+        ('rq', 'Requested'),
+        ('cf', 'Confirmed'),
+        ('op', 'Open'),
+        ('cp', 'Complete'),
+    )
+    status = models.CharField(
+        max_length=2,
+        choices=REQUEST_STATUS,
+        blank=True,
+        default='rq',
+        help_text='Request Status',
+    )
+
+    class Meta:
+        ordering = ['create_date', 'owner']
+
+    def __str__(self):
+        return f'{self.create_date}: {self.id} {self.owner}'
+
+
