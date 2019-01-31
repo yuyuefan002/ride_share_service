db1216587bd9b5afb8e355c662efed0fddf06c22
yuyuefan002 <yy225@vcm-5475.vm.duke.edu>
2019-01-31 16:01:43 -0500

    virtual environment

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/yy225/coursework/ece568/django/ride_share_service/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- orders/tests.py
+++ orders/tests.py
@@ -1,3 +1,3 @@
 from django.test import TestCase
-
+from .models import Request
 # Create your tests here.
