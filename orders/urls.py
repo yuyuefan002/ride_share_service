2074f264f1b4c8c78b146920c6a7b37c02d43370
jz270 <jz270@duke.edu>
2019-01-31 21:25:48 -0500

    new view name

--- orders/urls.py
+++ orders/urls.py
@@ -9,15 +9,15 @@ urlpatterns =[
     path('driver/profile-edit/', driver.ProfileEditor, name='driver_edit'),
     path('driver/register-error/', driver.RegisterErr, name='driver_register_error'),
     path('driver/ID-check/', driver.IDCheck, name='driver_check'),
-    path('driver/search-request/', driver.SearchingRequestListView.as_view(), name='ride_search'),
+    path('driver/search-request/', driver.SearchingRequestListView.as_view(), name='driver_search_ride'),
     path('driver/ongoing-request-detail/<uuid:pk>/', driver.OGINRideDetail, name='cf_ride_detail'),
     path('driver/confirm-request/<uuid:pk>/', driver.ConfirmRequest, name='ride_confirm'),
-
+    path('owner/ongoing-request-list/', driver.OGINRequestListView.as_view(), name='driver_order_list'),
 
     
-    path('owner/make-ride-request/', owner.MakeRequest, name='ride_request'),
-    path('owner/ongoing-request-list/', owner.OGINRequestListView.as_view(), name='cf_ride_status'),  
-    path('owner/requests/', owner.RequestListView.as_view(), name='requests'),   
+    path('owner/make-ride-request/', owner.MakeRequest, name='owner_request_ride'),
+   
+    path('owner/requests/', owner.RequestListView.as_view(), name='owner_order_list'),   
     path('owner/request-detail/<uuid:pk>/', owner.RequestDetail, name='cf_ride_request_check'),
     path('owner/requests/<uuid:pk>/', owner.RideRequestEditing, name='ride_request_editing'),
     
@@ -25,8 +25,8 @@ urlpatterns =[
     
     
 
-    path('sharer/make-ride-request/', sharer.MakeRequest, name='share_ride_request'),
-    path('sharer/ongoing-request-list/', sharer.RequestListView.as_view(), name='share_requests'),
+    path('sharer/make-ride-request/', sharer.MakeRequest, name='sharer_request_ride'),
+    path('sharer/ongoing-request-list/', sharer.RequestListView.as_view(), name='sharer_order_list'),
     path('sharer/request-detail/<uuid:pk>/', sharer.RequestDetail, name='cf_share_ride_request_check'),
     path('sharer/available-ride-list/<uuid:pk>/', sharer.AvailableRideSearchingListView.as_view(), name='share_ride_list'),
     path('sharer/join-ride/<uuid:main_id>/', sharer.JoinShareRide, name='middlepath'),
